de7a324b90b055e755c6550820ddfd72cf14a6f7
DonaldKnut <openiyiibrahim@gmail.com>
2024-02-03 12:04:43 +0100

    seventh commit

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,13 +4,15 @@ import { Resend } from "resend";
 // Initialize the 'resend' instance
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 // Function to send a verification email
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   // Send the verification email
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `${domain}`,
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
